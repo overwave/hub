9120155a29f493e972243ab632d4833f944c63c9
Lisa Gavryushkina <elizavetagavryushkina@yandex.ru>
2023-11-21 00:22:57 +0300

    refactor: printing

--- chess/src/main/kotlin/dev/overwave/chess/core/Dummy.kt
+++ chess/src/main/kotlin/dev/overwave/chess/core/Dummy.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service
 class Dummy : ApplicationListener<ApplicationReadyEvent> {
     override fun onApplicationEvent(event: ApplicationReadyEvent) {
         val initializer = Initializer()
-        initializer.printChessBoardLikeLizunya()
+        initializer.fillBoardLikeAPro()
         initializer.printChessBoardLikePro()
 
         }
--- chess/src/main/kotlin/dev/overwave/chess/core/Initializer.kt
+++ chess/src/main/kotlin/dev/overwave/chess/core/Initializer.kt
@@ -3,63 +3,29 @@ package dev.overwave.chess.core
 class Initializer {
 
     private val greeting = "Hello, let's play chess!"
-    private val board = Array(8) { Array(8) {""} }
+    private val board = Array(8) { Array(8) { "" } }
     private val blackFigures = arrayOf("♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜")
 
-    fun printChessBoardLikeLizunya() {
-        println(greeting)
-        println(ChessBoardParts.ALPHABETICAL_INDEXES.view)
-        println(ChessBoardParts.FRAME_TOP.view)
-        for(i in 8 downTo 2 step 2) {
-            println("${ChessBoardParts.EMPTY_CELLS_1.view} $i")
-            println("${ChessBoardParts.EMPTY_CELLS_2.view} ${i - 1}")
+
+    fun fillBoardLikeAPro() {
+        for ((y, row) in board.withIndex()) {
+            for (x in row.indices) {
+                board[y][x] = if ((x + y) % 2 == 0) "██" else "  "
+            }
         }
-        println(ChessBoardParts.FRAME_BOTTOM.view)
-        println(ChessBoardParts.ALPHABETICAL_INDEXES.view)
     }
 
     fun printChessBoardLikePro() {
-        for ((index, row) in board.withIndex()) {
-            if(index % 2 == 0) {
-                if(index == 7) {
-                    for (i in row.indices) {
-
-                    }
-
-
-                }
-                else {
-                    for (i in row.indices) {
-                        if(i % 2 == 0) {
-                            row[i] = "  "
-                        } else
-                            row[i] ="██"
-                    }
-                }
-            } else {
-                for (i in row.indices) {
-                    if (i % 2 == 0) {
-                        row[i] = "██"
-                    } else
-                        row[i] = "  "
-                }
-            }
-        }
-
-        val boardAsString = board.mapIndexed { index, row ->
-            val cells = row.joinToString("  ")
-            "║$cells║ ${index + 1}"
-        }
-
         println(greeting)
         println(ChessBoardParts.FRAME_TOP.view)
-
-        // 2️⃣
-        println(boardAsString.reversed().joinToString("\n"))
+        for ((y, row) in board.withIndex()) {
+            print("║")
+            print(row.joinToString(separator = ""))
+            print("║ ")
+            println(8 - y)
+        }
         println(ChessBoardParts.FRAME_BOTTOM.view)
         println(ChessBoardParts.ALPHABETICAL_INDEXES.view)
 
     }
-
-
 }
