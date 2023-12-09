cb218ac799d243b2749fd023697b1097f43a410d
Rodion Kabanov <overwave2@gmail.com>
2023-12-09 22:16:14 +0300

    session repository renamed because of conflict with spring security

--- chess/src/main/kotlin/dev/overwave/chess/service/GameSessionRepository.kt
+++ chess/src/main/kotlin/dev/overwave/chess/service/GameSessionRepository.kt
@@ -3,5 +3,5 @@ package dev.overwave.chess.service
 import dev.overwave.chess.model.Session
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface SessionRepository : JpaRepository<Session, Long> {
+interface GameSessionRepository : JpaRepository<Session, Long> {
 }
