4dfec84ce506aad3701d00b78162924f825ccd67
Rodion Kabanov <overwave2@gmail.com>
2023-12-10 04:15:01 +0300

    application.properties -> yaml

--- chess/src/test/kotlin/dev/overwave/chess/misc/UnitTest.kt
+++ chess/src/test/kotlin/dev/overwave/chess/misc/UnitTest.kt
@@ -2,19 +2,21 @@ package dev.overwave.chess.misc
 
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.TestConstructor
-import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-@ContextConfiguration(classes = [TestConfiguration::class])
+@ContextConfiguration(
+    classes = [TestConfiguration::class],
+    initializers = [ConfigDataApplicationContextInitializer::class]
+)
 @ExtendWith(SpringExtension::class)
 @AutoConfigureEmbeddedDatabase(
     provider = AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY,
     refresh = AutoConfigureEmbeddedDatabase.RefreshMode.AFTER_CLASS
 )
-@TestPropertySource(locations = ["classpath:application.properties"])
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 annotation class UnitTest
