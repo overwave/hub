1d70e58fcc759e9e383bfb87ae5d9c60e5fea761
Rodion Kabanov <overwave2@gmail.com>
2024-01-14 15:02:14 +0300

    fixed controller functional test

--- chess/src/test/kotlin/dev/overwave/chess/controller/GameControllerTest.kt
+++ chess/src/test/kotlin/dev/overwave/chess/controller/GameControllerTest.kt
@@ -1,36 +1,32 @@
 package dev.overwave.chess.controller
 
-import dev.overwave.chess.misc.UnitTest
+import dev.overwave.chess.misc.FunctionalTest
 import dev.overwave.chess.model.Session
 import dev.overwave.chess.model.SessionStatus
 import dev.overwave.chess.model.User
 import dev.overwave.chess.repository.SessionRepository
 import dev.overwave.chess.repository.UserRepository
-import org.junit.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
-@UnitTest
-@WebMvcTest(GameController::class)
-class GameControllerTest {
-    @Autowired
-    private lateinit var userRepository: UserRepository
-    @Autowired
-    private lateinit var sessionRepository: SessionRepository
-    @Autowired
-    private lateinit var mockMvc: MockMvc
-
+@FunctionalTest
+class GameControllerTest(
+    private val userRepository: UserRepository,
+    private val sessionRepository: SessionRepository,
+    private val mockMvc: MockMvc,
+) {
     @Test
+    @WithMockUser
     fun `when get open sessions then open sessions returned`() {
         val user = userRepository.save(User("login", "name", "ip", "password", false))
         val session = sessionRepository.save(Session(user, null, SessionStatus.OPEN))
         mockMvc.get("/chess/api/game/open").andExpect {
             status { isOk() }
             content { contentType(MediaType.APPLICATION_JSON) }
-            content { json("{}", true) }
+            content { json("[{\"id\":1,\"status\":\"OPEN\"}]", true) }
         }
     }
 }
--- chess/src/test/kotlin/dev/overwave/chess/misc/FunctionalTest.kt
+++ chess/src/test/kotlin/dev/overwave/chess/misc/FunctionalTest.kt
@@ -2,7 +2,10 @@ package dev.overwave.chess.misc
 
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.TestConstructor
 import org.springframework.test.context.junit.jupiter.SpringExtension
@@ -14,9 +17,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
     initializers = [ConfigDataApplicationContextInitializer::class]
 )
 @ExtendWith(SpringExtension::class)
+@SpringBootTest
+@AutoConfigureMockMvc
 @AutoConfigureEmbeddedDatabase(
     provider = AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY,
     refresh = AutoConfigureEmbeddedDatabase.RefreshMode.AFTER_CLASS
 )
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-annotation class UnitTest
+@ActiveProfiles("test")
+annotation class FunctionalTest
