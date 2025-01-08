f73d04e7a33b52f52f7a6e049243d0e769cf466f
Rodion Kabanov <overwave2@gmail.com>
2025-01-08 18:25:13 +0300

    style fixed

--- chess/src/main/kotlin/dev/overwave/chess/game/chess/GameController.kt
+++ chess/src/main/kotlin/dev/overwave/chess/game/chess/GameController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping(path = ["/chess/api/game/v2"], produces = [MediaType.APPLICATION_JSON_VALUE])
 class GameController(
-    private val gameService: GameService
+    private val gameService: GameService,
 ) {
     @PutMapping("/start")
     fun startGame(): Any = gameService.startGame()
